2cec3ff9a84099914ea452e9e4af50184bc44983
aditikrish <131293337+aditikrish@users.noreply.github.com>
2023-04-19 23:26:42 +0530

    Update 21f1004270_week8.py

--- 21f1004270_week8.py
+++ 21f1004270_week8.py
@@ -7,13 +7,6 @@ num2 = st.number_input("Enter 2nd number: ")
 num3 = st.number_input("Enter 3rd number: ")
 calc=st.button("Find the largest number")
 
-def largestNumber(num1,num2,num3):
-   if (num1 >= num2) and (num1 >= num3):
-      largest = num1
-   elif (num2 >= num1) and (num2 >= num3):
-      largest = num2
-   else:
-      largest = num3
 if calc:
-   largest=largestNumber(float(num1),float(num2),float(num3))
+   largest=max(float(num1),float(num2),float(num3))
    st.write("The largest Number is:",largest)
