4f389692cc0a5814bf685f23ed164075c096f6fb
aditikrish <131293337+aditikrish@users.noreply.github.com>
2023-04-19 23:45:29 +0530

    Update 21f1004270_week8.py

--- 21f1004270_week8.py
+++ 21f1004270_week8.py
@@ -3,8 +3,8 @@ st.title("Find the largest number among the given 3 numbers")
 
 
 num1 = st.number_input("Enter 1st number: ", step=0.5)
-num2 = st.number_input("Enter 2nd number: ", step=0.5)
-num3 = st.number_input("Enter 3rd number: ", step=0.5)
+num2 = st.number_input("Enter 2nd number: ", step=0.3)
+num3 = st.number_input("Enter 3rd number: ", step=0.2)
 calc=st.button("Find the largest number")
 
 if calc:
