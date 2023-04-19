ce6087094c4786219ef51b1936b7197936b1726e
aditikrish <131293337+aditikrish@users.noreply.github.com>
2023-04-19 23:06:12 +0530

    Create 21f1004270_week8.py

---
+++ 21f1004270_week8.py
@@ -0,0 +1,19 @@
+import streamlit as st
+st.title("Find the largest number among the given 3 numbers")
+
+
+num1 = st.number_input("Enter 1st number: ")
+num1 = st.number_input("Enter 2nd number: ")
+num1 = st.number_input("Enter 3rd number: ")
+calc=st.button("Find the largest number")
+
+def largestNumber(num1,num2,num3):
+   if (num1 >= num2) and (num1 >= num3):
+      largest = num1
+   elif (num2 >= num1) and (num2 >= num3):
+      largest = num2
+   else:
+      largest = num3
+if calc:
+   largest=largestNumber(num1,num2,num3)
+   st.write("The largest Number is:",largest)
